64d5c7e0703089b297afb3eebd26a5f475d77543
Brian Orandi <brianorandi@Brandis-Mac-Air.local>
2025-10-31 13:44:15 +0300

    Added hx-indicator and hx-on attributes to the contact table

--- htmx_contacthub/contacts/views.py
+++ htmx_contacthub/contacts/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Contact
+from django.db.models import Q
+import time
 
+@login_required
 def index(request):
-    context = {}
+    contacts = request.user.contacts.all().order_by("-created_at")
+    context = {'contacts': contacts}
     return render(request, 'contacts.html', context)
+
+@login_required
+def search_contacts(request):
+    time.sleep(1)
+    query = (request.GET.get('search', "")).strip()
+    contacts = request.user.contacts.filter(
+        Q(name__icontains=query) | Q(email__icontains=query)
+    ).order_by("-created_at")
+    return render(request, 'partials/contact-list.html', {"contacts": contacts})
