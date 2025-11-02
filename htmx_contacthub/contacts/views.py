fd7605b060b054027f22ffc7f7cdfb20afc1a38c
Brian Orandi <brianorandi97@gmail.com>
2025-11-02 16:24:46 +0300

    Create contact view

--- htmx_contacthub/contacts/views.py
+++ htmx_contacthub/contacts/views.py
@@ -4,6 +4,7 @@ from .models import Contact
 from django.db.models import Q
 from contacts.forms import ContactForm
 import time
+from django.views.decorators.http import require_http_methods
 
 @login_required
 def index(request):
@@ -22,3 +23,17 @@ def search_contacts(request):
         Q(name__icontains=query) | Q(email__icontains=query)
     ).order_by("-created_at")
     return render(request, 'partials/contact-list.html', {"contacts": contacts})
+
+@login_required
+@require_http_methods(["POST"])
+def create_contact(request):
+    form = ContactForm(request.POST)
+    if form.is_valid():
+        contact = form.save(commit=False) # don't save to the db yet
+        contact.user = request.user
+        contact.save()
+        return render(request, 'partials/contact-row.html')
+    else:
+        form = ContactForm()
+        
+    return render(request, 'partials/contact-row.html', {'form': form})
